0275a9502682c05d022c209d6cd3dcc1cecd2aa2
Dale Holstein <DaleHolstein@wifi_stu-10-38-69-237.uncc.edu>
2018-02-23 21:20:43 -0500

    beta 1

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+// API Routes
+router.use("/api", apiRoutes);
+
+// If no API routes are hit, send the React app
+router.use((req, res) => res.send("dale holstein vanderbilt III"));
+
+module.exports = router;
